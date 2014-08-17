ff9837c0c104944ef9c2762d2209c007021ea4c0
Peter L. Morrell <pmorrell@umn.edu>
2014-08-17 18:26:38 -0500

    Updated argument parsing

--- splitter/splitter.py
+++ splitter/splitter.py
@@ -4,30 +4,43 @@
 # so that it can be uploaded to S3 (Amazon) storage.
 # Sending the files to a specified S3 repository would be a nice addition.
 
-# Usage: python splitter.py -i filename
+# Usage: python splitter.py -i filename -s size
 
 # Needed for arguments below
 import sys
-# Needed for python call command to run UNIX split command
+# To get arguments
+import argparse
+# Needed for call command to run UNIX process
 from subprocess import call
 
-# The desired size of split files
-# Current set to 50 MB as small files are easier to upload over a shaky VPN
-size = '50'
-
-# Take filename from standard input
-# Filename will be used as the base name for the split files
-# Number of split files is determined by input file size
-flag = 0
-for arg in sys.argv:
-	if flag:
-		filename_in = arg
-		break
-	if arg == '-i':
-		flag = 1
-sys.stdin = open(filename_in, 'r')
+#####
+#   Defining arguments
+#####
+#   A description of the program
+DESCR = """A Python tool for easier file splitting."""
+#   Create a new argument parser
+Arguments = argparse.ArgumentParser(description=DESCR, add_help=True)
+#   Add some arguments
+Arguments.add_argument('-i',
+                '--infile',
+                metavar='INPUT_FILE',
+                type=argparse.FileType('r'),
+                #default=sys.stdin,
+                help='Input file',
+                required=True)
+#   Size of split files to create
+Arguments.add_argument('-s',
+                '--size',
+                metavar='SIZE',
+                default='50',
+                type=int,
+                help='Size of output files in MB',
+                required=False)
+#   And parse them
+ParsedArgs = Arguments.parse_args()
 
 # Call the shell utility 'split' with argument '-b' for split size
 # File names are take from the command line
-call(['split', '-b', size + 'm', sys.stdin.name, sys.stdin.name + '-part-'])
+# str() coerces the variable which is an integer to a string
+call(['split', '-b', str(ParsedArgs.size) + 'm', ParsedArgs.infile.name, ParsedArgs.infile.name + '-part-'])
 
