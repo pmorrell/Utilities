5c7f9d8410c09990c4a6eeaab868bf62f97a8763
Peter Morrell <pmorrell@umn.edu>
2016-11-15 00:15:06 -0600

    SRA download script

--- SRA_download.sh
+++ SRA_download.sh
@@ -2,22 +2,35 @@
 
 #PBS -l mem=12gb,nodes=1:ppn=1,walltime=24:00:00 
 #PBS -m abe 
-#PBS -M mfrodrig@umn.edu 
+#PBS -M pmorrell@umn.edu 
 #PBS -q lab
 
-# Collect file from command line
-#SRA_FILES=${HOME}/baseline/SRR_Acc_List-2.txt
-SRA_FILES=$1
+#    Collect file from command line
+#SRA_FILES=${HOME}/scratch/MBE_Barley/MBE_barley.txt
+SRA_FILES=${HOME}/scratch/MBE_Barley/MBE_Bowman.txt
 
-# Make sure the file exists
+#   Make sure the file exists
 if ! [[ -f "${SRA_FILES}" ]]
     then echo "Failed to find ${SRA_FILES}, exiting..." >&2
     exit 1
     fi 
 
-# Make the array using command substitution
-# declare -a SRA_ARRAY=($(cat "${SRA_FILES}")) 
-mapfile -t SRA_ARRAY < "$SRA_FILES"
+#   Make the array using command substitution
+declare -a SRA_ARRAY=($(cat "${SRA_FILES}")) 
 
-#Print the values of the array to screen
+#   Print the values of the array to screen
 printf '%s\n' "${SRA_ARRAY[@]}"
+
+#   location of SRA download script, from Tom Kono's Misc_Utils
+SRA_FETCH=${HOME}/Apps/TKono/Misc_Utils/SRA_Fetch.sh 
+
+#   directory where SRA files will be downloaded
+OUTPUT=${HOME}/scratch/MBE_Barley/
+
+#   iterate over every each of the run numbers in a lit of SRA files
+#   and download to specified directory
+#   in SRA_Fetch -r = run #, -e experiment #, -p sample #, -s study #
+for i in "${SRA_ARRAY[@]}"
+    do bash $SRA_FETCH -r $i -d $OUTPUT
+    done
+
