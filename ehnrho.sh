13e9521c6c044e47458a1de40e8b96f1854045cd
Peter Morrell <pmorrell@umn.edu>
2017-09-26 16:25:52 -0500

    tuning for qsub

--- ehnrho.sh
+++ ehnrho.sh
@@ -1,16 +1,15 @@
 #!/bin/env bash
 
-#PBS -l mem=2500mb,nodes=1:ppn=16,walltime=96:00:00
+#PBS -l mem=2500mb,nodes=1:ppn=16,walltime=23:59:59
 #PBS -m abe
 #PBS -M pmorrell@umn.edu
-#PBS -q mesabi
+#PBS -q lab
 
 set -euo pipefail
 
 #    Generate new lookup tables for Hudson's two-site likelihood approach
 #    Peter L. Morrell, 26 September 2017
 #    Dependencies: ehnrho.c http://home.uchicago.edu/rhudson1/source/twolocus/programs/ehnrho.html
-
 EH=/panfs/roc/groups/9/morrellp/pmorrell/Apps/RHudson/ehnrho/eh
 
 POP_NUMBER=1
@@ -19,7 +18,7 @@ RHO_VALUES=25
 GROWTH_RATE=0.00000
 ITERATIONS=15000000
 
-OUT_FILE=h${SAMPLE_NUMBER}rho
+OUT_FILE=/panfs/roc/groups/9/morrellp/pmorrell/Workshop/ehnrho/h${SAMPLE_NUMBER}rho
 
 $EH $POP_NUMBER $SAMPLE_NUMBER $RHO_VALUES \
 0.000000 0.050000 0.100000 0.200000 0.300000 \
