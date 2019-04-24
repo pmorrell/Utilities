4aec031dbaa432f732047f2c8bc59815e64b04db
Peter Morrell <pmorrell@Peters-MacBook-Air.local>
2019-04-24 09:23:13 -0500

    a working version

---
+++ tarchiver.py
@@ -0,0 +1,44 @@
+#/usr/bin/env python3
+
+# 04 October 2018, Falcon Heights, MN
+# Peter L. Morrell - tired of manually creating tarred and bzipped archives
+# 24 April 2019, Falcon Heights, MN
+# Returned to this and go it to work!
+
+# Needed for arguments below
+import os
+import sys
+import tarfile
+
+Usage = """
+
+Usage:
+python archiver.py directory_name
+"""
+
+if len(sys.argv) < 1:
+    print(Usage)
+    exit(1)
+
+#####
+#   Defining arguments
+#####
+#   A description of the program
+DESCR = """A Python tool for archiving directories. Pass a directory as the
+only argument. The original directory is not modified, but a new file that is
+tarred and compressed with bzip2 is created."""
+#   Create a new argument parser
+
+try:
+    directory_name = sys.argv[1]
+    #print(directory_name)
+except:
+    print('Please pass directory_name')
+
+
+tar = tarfile.open( directory_name + ".tar.bz2", "w:bz2" )
+os.chdir( directory_name )
+for name in os.listdir( directory_name ):
+    #print(name)
+    tar.add(name)
+tar.close()
