16167b41ed4ace05416840d188e7a787dc2274e4
Peter Morrell <pmorrell@umn.edu>
2021-07-15 14:15:06 -0500

    improving layout

--- mosdepth.sh
+++ mosdepth.sh
@@ -20,14 +20,18 @@ MOSDEPTH=/panfs/roc/groups/9/morrellp/shared/Software/mosdepth/mosdepth
 OUT_PREFIX=WBDC355_Nanopore
 
 # Output directory
-WORK=/scratch.global/pmorrell/WBDC355_Nanopore
+OUT_DIR=/scratch.global/pmorrell/WBDC355_Nanopore
 
 # The size of the window in bp for coverage estimation.
 INCREMENT=500
 
 BAM=/panfs/roc/groups/9/morrellp/shared/Projects/WBDC_inversions/nanopore/WBDC_355/WBDC_combined_1_10/WBDC_355_v3/bam_file/WBDC_355_1_10_v3_sorted.bam
 
-# Change to the working directory and run esimation.
+# Check if our dir exists, if not make it
+mkdir -p ${OUT_DIR}
+
+# Go into the 
+cd ${OUT_DIR}
+
 # The threads option is for compression. Don't increase beyond 4, it doesn't speedup the program!
-cd "$WORK"
 $MOSDEPTH --no-per-base --threads 4 --fast-mode --by $INCREMENT $OUT_PREFIX $BAM
