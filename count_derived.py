2190a4328805bc42417ffeef120df4375755522a
Peter Morrell <pmorrell@umn.edu>
2021-07-27 13:16:48 -0500

    handling edge cases in genotyping

--- count_derived.py
+++ count_derived.py
@@ -15,29 +15,46 @@ import pandas as pd
 
 
 def allele_freq(genos):
+    freq = 0
     """Count frequency of the derived variant."""
     # The line below uses a pandas function, this is a little fragile!
     counts = dict(pd.value_counts(genos))
 # Missing genotypes need to be subtracted to get an accurate derived frequency.
-    missing_key = 'NN'
-    for missing_key in counts:
-        missing_count = counts['NN']
-        if missing_count == len(genos):
+    missing_key = ['NN']
+    ancest_key = ['AA']
+    het_key = ['AD']
+    der_key = ['DD']
+    for keys in counts:
+        temp_keys = [*counts]
+        #    If nothing but missing data, go to next SNP
+        if temp_keys == missing_key:
             continue
+        #    If nothing but ancestral state, go to next SNP
+        elif temp_keys == ancest_key:
+            continue
+        #    If nothing but missing data and ancestral state, go to next SNP!
+        #    The sum function flattens the nested lists
+        elif temp_keys == sum([ancest_key, missing_key], []):
+                continue
         else:
-            observ = (len(genos) * 2) - (counts['NN'] * 2)
-    else:
-        observ = (len(genos) * 2)
+            #    Calculate sample sizes adjusting for missing data
+            for missing_key[0] in counts:
+                missing_count = counts[missing_key[0]]
+                #   Again, if nothing but missing data (shouldn't get here!)
+                if missing_count == len(genos):
+                    continue
+                elif missing_count > 0:
+                    observ = (len(genos) * 2) - (counts[missing_key[0]] * 2)
+                else:
+                    observ = (len(genos) * 2)
     # Count heterozygotes so those genotypes also get an accurate count.
-    het_key = 'AD'
-    derive_key = 'DD'
-    if het_key in counts:
-        if derive_key not in counts:
-            freq = counts['AD'] / observ
+        if het_key[0] in counts:
+            if der_key[0] not in counts:
+                freq = counts[het_key[0]] / observ
+            elif der_key[0] in counts:
+                freq = (counts[het_key[0]] + (counts[der_key[0]] * 2)) / observ
         else:
-            freq = (((counts['AD']) + (counts['DD'] * 2)) / observ)
-    else:
-        freq = (counts['DD'] * 2 / observ)
+            freq = (counts[der_key[0]] * 2) / observ
     return freq
 
 
