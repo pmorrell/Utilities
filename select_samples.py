cd984c8eb96ef0142689720243f91c6b21db3771
Peter Morrell <pmorrell@umn.edu>
2021-08-28 13:40:31 -0500

    updating file naming

--- select_samples.py
+++ select_samples.py
@@ -18,10 +18,13 @@ def read_list(s):
                 sample_list.append(line.strip())
     return sample_list
 
-fn = sys.argv[1]
-basename = os.path.basename(fn)
-basename = os.path.splitext(basename)[0]
-sample_file = basename + "anc.txt.gz"
+
+def file_name(f):
+    base = os.path.basename(f)
+    #    Remove the first extension
+    base = os.path.splitext(base)[0]
+    basename = os.path.splitext(base)[0]
+    return basename
 
 
 def main(samples, ancestral):
@@ -30,9 +33,13 @@ def main(samples, ancestral):
     retain = read_list(samples)
     df = pd.read_csv(ancestral, compression='gzip', header=0, sep='\t')
     df_out = df[["Chromosome", "Pos", "SNPID", "Ancestral", "Derived", "Reference"] + retain]
-    df_out.to_csv(sample_file, sep='\t', index = False, compression = 'gzip')
+    sample, variant_class = file_name(samples, ancestral)
+    variant_class = file_name(ancestral)
+    sample_file = sample + '_' + variant_class + 'anc.txt.gz'
+    df_out.to_csv(sample_file, sep='\t', index=False, compression='gzip')
 
-if len(sys.argv) != 3:
+
+if len(sys.argv) <= 2:
     print("""Take a list of samples to cut down and the large file of \
     ancestral state for each individual and produces table with only the \
     listed samples retained.
