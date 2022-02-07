5f0f23639e5f0b2ed172084d3dc51b1dfecdc772
Peter Morrell <pmorrell@ra-full-mfa.vpn.umn.edu>
2022-02-07 15:42:25 -0600

    samplot script

--- samplot.sh
+++ samplot.sh
@@ -17,18 +17,19 @@ module python3/3.7.4_anaconda2019.10
 conda activate samplot
 
 cd /panfs/roc/groups/9/morrellp/pmorrell/Workshop/
-VCF=/panfs/roc/groups/9/morrellp/pmorrell/Workshop/M01_temp.vcf
+VCF=/panfs/roc/groups/9/morrellp/shared/Projects/Mutant_Barley/longranger_morex_v2/combined_mutated/Filtered/deletions/M01_singletons_only-scored_DEL_gte75Sup.vcf
 OUT_DIR=/panfs/roc/groups/9/morrellp/pmorrell/Workshop/Barley_Mutants
-GFF3=/panfs/roc/groups/9/morrellp/shared/References/Reference_Sequences/Barley/Morex_v2/gene_annotation/Barley_Morex_V2_gene_annotation_PGSB.all.sorted.gff3
+GFF3=/panfs/roc/groups/9/morrellp/shared/References/Reference_Sequences/Barley/Morex_v2/gene_annotation/Barley_Morex_V2_gene_annotation_PGSB.all.parts.sorted.gff3.gz
 
 #Samplot command line
 samplot vcf \
 --vcf ${VCF} \
+--sample_ids Morex_Nanopore Morex_10X M01_Nanopore M01_10X \
 --out-dir ${OUT_DIR} \
 --output_type png \
 --gff3 ${GFF3} \
 --bam /panfs/roc/groups/9/morrellp/shared/Projects/WBDC_inversions/nanopore/Morex/Morex_run1_to_run14/Morex_nanopore_1_14_V2_parts/Morex_1_14_align_V2_sorted_parts.bam \
 /panfs/roc/groups/9/morrellp/shared/Projects/Mutant_Barley/longranger_morex_v2/morex-sample2/morex-sample2_phased_possorted_bam.bam \
+/panfs/roc/groups/9/morrellp/shared/Datasets/Alignments/nanopore_mutated_barley/sam_processing/M01-3-3-12-41_run1-3.bam \
 /panfs/roc/groups/9/morrellp/shared/Projects/Mutant_Barley/longranger_morex_v2/M01-3-3/M01-3-3_phased_possorted_bam.bam \
-/panfs/roc/groups/9/morrellp/shared/Datasets/Alignments/nanopore_mutated_barley/sam_processing/M01-3-3-12-41_run1-3.bam
 >samplot_commands.sh
