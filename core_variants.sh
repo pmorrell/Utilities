4efb8083ea2181aae705a3b21795ce0721899ce3
Peter Morrell <pmorrell@umn.edu>
2022-02-17 12:07:39 -0600

    changing to single searches

--- core_variants.sh
+++ core_variants.sh
@@ -21,19 +21,31 @@ module load bcftools/1.6
 #    We can
 
 GFF3=/panfs/roc/groups/9/morrellp/shared/Datasets/Cowpea_Pan/VunguiculataIT97K-499-35_v1.2/annotation/Vunguiculata_IT97K-499-35_v1.2.gene.gff3.gz
-VCF=/panfs/roc/groups/9/morrellp/shared/Datasets/Cowpea_Pan/VunguiculataIT97K-499-35_v1.2/SNPs/IT97K_combined_genotype_snps_filtered.g.vcf.gz
+#VCF=/panfs/roc/groups/9/morrellp/shared/Datasets/Cowpea_Pan/VunguiculataIT97K-499-35_v1.2/SNPs/IT97K_combined_genotype_snps_filtered.g.vcf.gz
 OUT_DIR=/scratch.global/pmorrell/Cowpea_pan
 CORE=/panfs/roc/groups/9/morrellp/shared/Datasets/Cowpea_Pan/ITKcore.txt
 NONCORE=/panfs/roc/groups/9/morrellp/shared/Datasets/Cowpea_Pan/ITKnoncore.txt
+
+readarray -t CORE_genes < ${CORE}
+readarray -t NONCORE_genes < ${NONCORE}
+
 
 #    Cut the GFF3 file down to just gene positions to reduce our search space
 #    The GFF3 file was compressed already
 GENES=$(zgrep 'gene' ${GFF3})
 
 #    Create a sorted bed file with only CORE or only NONCORE gene positions
-CORE_POS=$(grep -E -f "${CORE}" <(echo "${GENES}") | cut -f 1,4,5 | sort -k1,1 -k2,2n -k3,3n)
-NONCORE_POS=$(grep -E -f "${NONCORE}" <(echo "${GENES}") | cut -f 1,4,5| sort -k1,1 -k2,2n -k3,3n)
+#CORE_POS=$(grep -E -f "${CORE}" <(echo "${GENES}") | cut -f 1,4,5 | sort -k1,1 -k2,2n -k3,3n)
+#NONCORE_POS=$(grep -E -f "${NONCORE}" <(echo "${GENES}") | cut -f 1,4,5| sort -k1,1 -k2,2n -k3,3n)
+for i in "${CORE_genes[@]}"
+    do
+    grep -E "${i}" <(echo "${GENES}") | cut -f 1,4,5 | sort -k1,1 -k2,2n -k3,3n >${OUT_DIR}/CORE_POS.txt
+done
 
+for j in "${NONCORE_genes[@]}"
+    do
+    grep -E "${j}" <(echo "${GENES}") | cut -f 1,4,5| sort -k1,1 -k2,2n -k3,3n >${OUT_DIR}/NONCORE_POS.txt
+  done
 #    Use bcftools to cut the VCF down to individual SNPs within genes from each list
-bcftools view --regions-file <(echo "${CORE_POS}") ${VCF} --output-type z --output-file ${OUT_DIR}/CORE_vcf.gz
-bcftools view --regions-file <(echo "${NONCORE_POS}") ${VCF} --output-type z --output-file ${OUT_DIR}/NONCORE_vcf.gz
+#bcftools view --regions-file ${OUT_DIR}/CORE_POS.txt ${VCF} --output-type z --output-file ${OUT_DIR}/CORE_vcf.gz
+#bcftools view --regions-file ${OUT_DIR}/NONCORE_POS.txt ${VCF} --output-type z --output-file ${OUT_DIR}/NONCORE_vcf.gz
