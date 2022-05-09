e87b97c08744589089b8ea655b8cea57b3e5d0df
Peter Morrell <pmorrell@umn.edu>
2022-05-09 16:20:12 -0500

    adding exlusion list

--- smoove.sh
+++ smoove.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 #SBATCH --time=8:00:00
-#SBATCH --ntasks=8
+#SBATCH --ntasks=2
 #SBATCH --mem=200g
 #SBATCH --tmp=200g
 #SBATCH --mail-type=ALL
@@ -9,16 +9,23 @@
 set -e
 set -o pipefail
 
+export PATH="$PATH:/panfs/roc/groups/9/morrellp/shared/Software/smoove/"
+
 # Load the tools needed
 module load samtools/1.9
 module load htslib/1.9
+module load bcftools/1.10.2
+module load python2/2.7.12_anaconda4.2
 # The application and associated tools are all in the directory below
-SMOOVE=/panfs/roc/groups/9/morrellp/shared/Software/smoove
+SMOOVE=/panfs/roc/groups/9/morrellp/shared/Software/smoove/smoove
 OUT_DIR=/panfs/roc/groups/9/morrellp/pmorrell/Workshop/Soy_120/smoove/out_dir
-
-# BAM file, reference, and
-BAM=/panfs/roc/groups/9/morrellp/shared/Datasets/10x_Genomics/Soybean/m92_220/M92_220_phased_possorted_bam.bam
+EXCLUDE=/panfs/roc/groups/9/morrellp/pmorrell/Workshop/Soy_120/Gmax_275_v2.0.fa_exclude.bed
 reference_fasta=/panfs/roc/groups/9/morrellp/shared/References/Reference_Sequences/Soybean/PhytozomeV11/Gmax/assembly/Gmax_275_v2.0.fa
+
+# BAM file, reference, and processor count
+# Only 1 or 2 threads are useful
+BAM=/panfs/roc/groups/9/morrellp/shared/Datasets/10x_Genomics/Soybean/m92_220/M92_220_phased_possorted_bam.bam
+SAMPLE_NAME=M92_220  
 threads=2
 
 # Check if our dir exists, if not make it
@@ -28,4 +35,4 @@ mkdir -p ${OUT_DIR}
 cd ${OUT_DIR}
 
 # A minimum set of positions for calling variants. Not including an "excludes" file with problematic regions
-$SMOOVE call -x --name M92_220 --fasta  $reference_fasta -p $threads --genotype $BAM
+${SMOOVE} call -x --name ${SAMPLE_NAME} --exclude ${EXCLUDE} --fasta  ${reference_fasta} -p ${threads} --genotype ${BAM}
